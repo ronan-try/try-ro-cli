5c2c02df853da6c0eb32449404ee94af7dcd4122
Ronan <liruonan@hotmail.com>
2021-03-23 20:36:55 +0800

    feat(cli-shared-utils): add chalk

--- packages/@ro/cli-shared-utils/index.ts
+++ packages/@ro/cli-shared-utils/index.ts
@@ -1,2 +1,12 @@
 
 export { trimOnlyEnd } from './lib/trimOnlyEnd';
+
+export {
+  textRed,
+  textCyan,
+  textYellow,
+  textGreen,
+  textRedBright,
+  textCyanBright,
+  textGray,
+} from './lib/chalk';
---
+++ packages/@ro/cli-shared-utils/lib/chalk.ts
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+
+export const textRed = (msg: string) => chalk.red(msg);
+
+export const textCyan = (msg: string)=> chalk.cyan(msg);
+
+export const textYellow = (msg: string) => chalk.yellow(msg);
+
+export const textGreen = (msg: string) => chalk.green(msg);
+
+export const textRedBright = (msg: string) => chalk.redBright(msg);
+
+export const textCyanBright = (msg: string) => chalk.cyanBright(msg);
+
+export const textGray = (msg: string) => chalk.gray(msg);
