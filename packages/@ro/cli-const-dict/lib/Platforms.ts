39aff8efc2f2d382530531560728d7c122bdf19f
Ronan <liruonan@hotmail.com>
2021-03-24 13:08:12 +0800

    update: 纠正常量命名规范

--- packages/@ro/cli-const-dict/lib/Platforms.ts
+++ packages/@ro/cli-const-dict/lib/Platforms.ts
@@ -1,13 +1,13 @@
 
 /** git 平台 */
-export const GitPlatform = {
+export const GIT_PLATFORMS = {
   GitHub: 'github',
   GitLab: 'gitlab',
   Gitee: 'gitee',
 };
 
 /** 操作系统平台 */
-export const OSPlatform = {
+export const OS_PLATFORMS = {
   Win32: 'win32',
   Darwin: 'darwin',
   Linux: 'linux',
