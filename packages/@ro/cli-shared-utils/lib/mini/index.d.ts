3711c16e4d75eff9c74e4013182dc830fe2bc685
Ronan <liruonan@hotmail.com>
2021-03-24 23:56:01 +0800

    build(cli-shared-utils):

---
+++ packages/@ro/cli-shared-utils/lib/mini/index.d.ts
@@ -0,0 +1,2 @@
+export { trimOnlyEnd } from './src/trimOnlyEnd';
+export { textRed, textCyan, textYellow, textGreen, textRedBright, textCyanBright, textGray, } from './src/chalkText';
---
+++ packages/@ro/cli-shared-utils/lib/mini/src/trimOnlyEnd.d.ts
@@ -0,0 +1,2 @@
+/** 字符串trimEnd, 区别于lodash.trimEnd */
+export declare const trimOnlyEnd: (str: string, chars: string) => string;
