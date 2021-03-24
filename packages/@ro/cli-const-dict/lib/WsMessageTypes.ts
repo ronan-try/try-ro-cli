e55f5567ad49c9e7a277417ccf659c9209b9445a
Ronan <liruonan@hotmail.com>
2021-03-24 13:12:04 +0800

    update: 修正常量命名规范

--- packages/@ro/cli-const-dict/lib/WsMessageTypes.ts
+++ packages/@ro/cli-const-dict/lib/WsMessageTypes.ts
@@ -1,28 +1,28 @@
 
 /** 已连接消息类型 */
-export const Connected = 'connected';
+export const CONNECTED = 'connected';
 
 /** 测试消息类型 */
-export const Test = 'test';
+export const TEST = 'test';
 
 /** 获取缓存项目信息 */
-export const CacheProjects = 'cacheProjects';
+export const CACHE_PROJECTS = 'cacheProjects';
 
 /** 添加目标Upstream */
-export const AddTargetUpstream = 'addTargetUpstream';
+export const ADD_TARGET_UPSTREAM = 'addTargetUpstream';
 
 /** 获取 ro cli upstream 仓库 */
-export const FetchRocliUpstream = 'fetchRocliUpstream';
+export const FETCH_ROCLI_UPSTREAM = 'fetchRocliUpstream';
 
 /** 当前git 分支 */
-export const GitBranchR = 'gitBranchR';
+export const GIT_BRANCH_R = 'gitBranchR';
 
-export const GitFork = 'gitFork';
+export const GIT_FORK = 'gitFork';
 
-export const GitTrack = 'gitTrack';
+export const GIT_TRACK = 'gitTrack';
 
-export const GitSync = 'gitSync';
+export const GIT_SYNC = 'gitSync';
 
-export const OpenWithVSCode = 'openWithVSCode';
+export const OPEN_WITH_VSCODE = 'openWithVSCode';
 
-export const OpenWithFolder = 'openWithFolder';
+export const OPEN_WITH_FOLDER = 'openWithFolder';
