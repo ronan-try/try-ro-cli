a8fa51e88279577816bb12d864ab3b4ad0253038
Ronan <liruonan@hotmail.com>
2021-03-25 23:30:50 +0800

    update: cli pr

--- packages/@ro/cli/lib/pr.js
+++ packages/@ro/cli/lib/pr.js
@@ -7,7 +7,10 @@
 // 交互模块
 const inquirer = require('inquirer');
 // 内部依赖
-const { textRedBright } = require('@ro/cli-shared-utils/lib/umd/chalkText');
+const { 
+  textRedBright,
+  textGreen,
+} = require('@ro/cli-shared-utils/lib/umd/chalkText');
 const { logStep } = require('@ro/cli-shared-utils/lib/umd/logStep');
 const {
   existGitRepo,
@@ -15,6 +18,29 @@ const {
   gitLocalOriginURI,
 } = require('@ro/cli-service/lib/umd/git');
 // // require('../cli-editors/index').openWithBroswer(url);
+
+async function sureToMakeMRFromTheBranch(branchName) {
+  const questions = [{
+    type: 'confirm',
+    message: 'Sure to Make a MR/PR from the branch: ' + textGreen(branchName),
+    name: 'yeah'
+  }];
+  const { yeah } = await inquirer.prompt(questions);
+  return yeah;
+}
+
+async function openBroswerWithMrUrl(workPath, branchName) {
+  const originUrl = await gitLocalOriginURI(workPath);
+  const url =
+    `${originUrl}`
+      .replace('.com:', '.com/')
+      .replace('git@', 'https://')
+      .replace('.git', '/merge_requests/new?')
+    + 'merge_request%5Bsource_branch%5D='
+    + branchName;
+
+  require('shelljs').exec('open ' + url);
+}
 
 module.exports = async () => {
   logStep`Hi, Here is MR `;
@@ -26,39 +52,25 @@ module.exports = async () => {
   if (!existedGitRepo) {
     console.log(textRedBright`当前路径：`, curWorkPath);
     console.log(textRedBright`不存在 git repo`);
-    process.exit(1);
 
     logStep('step2: 不含Git Repo');
     logStep('step3: 选择Git Repo');
     logStep('step4: 选择branch');
     logStep('step5: 发起MR');
+    process.exit(1);
   }
 
   logStep`step2: 含Git repo`;
   logStep`step3: 当前Gir repo 是否在config中？`;
   // 当前仅考虑内部使用，所以跳过step3
   logStep('step4: 当前branch是否为要MR的branch？');
-  const curBranchName = await gitBranchCurrent(curWorkPath);
   {
-    const questions = [{
-      type: 'confirm',
-      message: 'Make a MR/PR from current branch: ' + textRedBright(curBranchName),
-      name: 'yeah'
-    }];
-    const { yeah } = await inquirer.prompt(questions);
+    const curBranchName = await gitBranchCurrent(curWorkPath);
+    const yeah = await sureToMakeMRFromTheBranch(curBranchName);
     if (yeah) {
       logStep('step5: 就是当前的branch，发起MR');
 
-      const originURI = await gitLocalOriginURI(curWorkPath);
-      const url =
-        `${originURI}`
-          .replace('.com:', '.com/')
-          .replace('git@', 'https://')
-          .replace('.git', '/merge_requests/new?') +
-        'merge_request%5Bsource_branch%5D=' +
-        curBranchName;
-
-      require('shelljs').exec('start ' + url);
+      openBroswerWithMrUrl(curWorkPath, curBranchName);
     } else {
       // to do
     }
