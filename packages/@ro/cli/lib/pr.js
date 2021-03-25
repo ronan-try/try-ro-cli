74624f65e6a0255f57cdf0c211a8ff9691e55ddb
Ronan <liruonan@hotmail.com>
2021-03-25 12:22:44 +0800

    update(cli/pr.js): demo发起mr

--- packages/@ro/cli/lib/pr.js
+++ packages/@ro/cli/lib/pr.js
@@ -8,9 +8,20 @@
 const inquirer = require('inquirer');
 // 内部依赖
 const { textRedBright } = require('@ro/cli-shared-utils/lib/umd/chalkText');
-const {} = require('@ro/cli-shared-utils/lib/umd/')
+const { logStep } = require('@ro/cli-shared-utils/lib/umd/logStep')
 // // require('../cli-editors/index').openWithBroswer(url);
 
 module.exports = () => {
   console.log(textRedBright('hi, mr'));
+  logStep('Hi, Here is MR');
+
+  const curLocalBranch = 'feat_ng2vue_stand_approval';
+  // const targetRepo = 'https://gitlab.ziroom.com/design-fe/fe-data-center/moonfall.git';
+  const originRepo = 'https://gitlab.ziroom.com/lirn2/moonfall.git';
+
+  const url = `${originRepo}`.replace('.git', '/merge_requests/new?') +
+    'merge_request%5Bsource_branch%5D=' +
+    curLocalBranch;
+
+  require('shelljs').exec('start ' + url);
 };
