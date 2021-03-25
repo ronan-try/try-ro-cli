c0c314e165bdc9ca28cc0b8a17592f02fe3f003c
Ronan <liruonan@hotmail.com>
2021-03-25 13:27:05 +0800

    update(cli pr): 规划logStep

--- packages/@ro/cli/lib/pr.js
+++ packages/@ro/cli/lib/pr.js
@@ -12,8 +12,19 @@ const { logStep } = require('@ro/cli-shared-utils/lib/umd/logStep')
 // // require('../cli-editors/index').openWithBroswer(url);
 
 module.exports = () => {
-  console.log(textRedBright('hi, mr'));
   logStep('Hi, Here is MR');
+
+  logStep('step1: 识别当前执行环境，是否含Git Repo');
+
+  logStep('step2: 含Git repo');
+  logStep('step3: 当前branch是否为要MR的branch？');
+  logStep('step4: 就是当前的branch，发起MR');
+
+
+  logStep('step2: 不含Git Repo');
+  logStep('step3: 选择Git Repo');
+  logStep('step4: 选择branch');
+  logStep('step5: 发起MR');
 
   const curLocalBranch = 'feat_ng2vue_stand_approval';
   // const targetRepo = 'https://gitlab.ziroom.com/design-fe/fe-data-center/moonfall.git';
