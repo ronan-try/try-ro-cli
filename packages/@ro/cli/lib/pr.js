0b76b0bbc6696373f7eb508da0a5f1c2fe4ee53f
Ronan <liruonan@hotmail.com>
2021-03-25 12:08:55 +0800

    build(cli-shared-utils):

--- packages/@ro/cli/lib/pr.js
+++ packages/@ro/cli/lib/pr.js
@@ -8,7 +8,7 @@
 const inquirer = require('inquirer');
 // 内部依赖
 const { textRedBright } = require('@ro/cli-shared-utils/lib/umd/chalkText');
-
+const {} = require('@ro/cli-shared-utils/lib/umd/')
 // // require('../cli-editors/index').openWithBroswer(url);
 
 module.exports = () => {
