c986cbdee2943fe37e51f3e03cd4edd3e18c004a
Ronan <liruonan@hotmail.com>
2021-04-11 19:56:43 +0800

    update(vscode-extension): react 项目可以起来，但图片加载失败

--- packages/ro-vscode-extension/ext-src/extension.ts
+++ packages/ro-vscode-extension/ext-src/extension.ts
@@ -1,5 +1,5 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
+import ReactPanel from './ReactPanel';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "ro-vscode-extension" is now active!');
@@ -34,8 +34,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(openWebview);
 
-	let reactview = vscode.commands.registerCommand('ro-vscode-extension.react-webview.start', () => {
-		ReactPanel.createOrShow(context.extensionPath);
+	let reactview = vscode.commands.registerCommand('ro-vscode-extension.startReactWebview', () => {
+		ReactPanel.createOrShow(context.extensionUri, context.extensionPath);
 	});
 	context.subscriptions.push(reactview);
 }
@@ -58,108 +58,4 @@ function getWebviewContent() {
 			</body>
 		</html>
 	`;
-}
-
-class ReactPanel {
-	public static currentPanel: ReactPanel | undefined;
-
-	private static readonly viewType = 'react';
-
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _extensionPath: string;
-	private _disposables: vscode.Disposable[] = [];
-
-	public static createOrShow(extensionPath: string) {
-		const column = vscode.window.activeTextEditor
-			? vscode.window.activeTextEditor.viewColumn
-			: undefined
-			;
-
-		if (ReactPanel.currentPanel) {
-			ReactPanel.currentPanel._panel.reveal(column);
-		} else {
-			ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
-		}
-	}
-
-	private constructor(extensionPath: string, column: vscode.ViewColumn) {
-		this._extensionPath = extensionPath;
-
-		this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, 'React', column, {
-			enableScripts: true,
-			localResourceRoots: [
-				vscode.Uri.file(path.join(this._extensionPath, 'build'))
-			]
-		});
-
-		this._panel.webview.html = this._getHtmlForWebview();
-
-		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-
-		this._panel.webview.onDidReceiveMessage(message => {
-			switch (message.command) {
-				case 'alert':
-					vscode.window.showErrorMessage(message.text);
-					return;
-			}
-		}, null, this._disposables);
-	}
-
-	public doRefactor() {
-		this._panel.webview.postMessage({ command: 'refactor' });
-	}
-
-	public dispose() {
-		ReactPanel.currentPanel = void 0;
-
-		this._panel.dispose();
-
-		while (this._disposables.length) {
-			const x = this._disposables.pop();
-			x && x.dispose();
-		}
-	}
-
-	private _getHtmlForWebview() {
-		const manifest = require(path.join(this._extensionPath, 'build', 'asset-manifest.json'));
-		const mainScript = manifest['main.js'];
-		const mainStyle = manifest['main.css'];
-
-		const scriptPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainScript));
-		const scriptUri = scriptPathOnDisk.with({ scheme: 'vscode-resource' });
-		const stylePathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainStyle));
-		const styleUri = stylePathOnDisk.with({ scheme: 'vscode-resource' });
-
-		const nonce = getNonce();
-
-		return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="utf-8">
-				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-				<meta name="theme-color" content="#000000">
-				<title>React App</title>
-				<link rel="stylesheet" type="text/css" href="${styleUri}">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
-				<base href="${vscode.Uri.file(path.join(this._extensionPath, 'build')).with({ scheme: 'vscode-resource' })}/">
-			</head>
-
-			<body>
-				<h1>This is React Page.</h1>
-				<noscript>You need to enable JavaScript to run this app.</noscript>
-				<div id="root"></div>
-				
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
-	}
-}
-
-function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
 }
