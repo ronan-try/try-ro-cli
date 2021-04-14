8b8e0e9a0afb789633055ef2d15a0e37a064c74f
Ronan <liruonan@hotmail.com>
2021-04-15 00:57:20 +0800

    feat(mr.ts): gitextension

--- ro-cli-vscode-extension/src/registerCommands/mr.ts
+++ ro-cli-vscode-extension/src/registerCommands/mr.ts
@@ -1,7 +1,26 @@
 import * as vscode from 'vscode';
 
+import { GitExtension } from '../builted-in/vscode.git/git';
+
 const command = vscode.commands.registerCommand('ro-cli-vscode-extension.mr', () => {
-  vscode.window.showInformationMessage('Hello World from @ro/cli-vscode-extension!');
+  const folders = vscode.workspace.workspaceFolders;
+  if (!Array.isArray(folders)) {
+    vscode.window.showErrorMessage('ro mr 没有监听到有效的路径-0');
+    return;
+  }
+  if (folders.length < 1) {
+    vscode.window.showErrorMessage('ro mr 没有监听到有效的路径-1');
+    return;
+  }
+
+  const curFolder = folders[0] as vscode.WorkspaceFolder;
+
+  const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
+  const git = gitExtension?.getAPI(1);
+
+  const state = git?.getRepository(curFolder.uri)?.state;
+  
+  vscode.window.showInformationMessage('remotes: ' + state?.remotes.length);
 });
 
 export default command;
